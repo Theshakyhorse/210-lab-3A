5f936fe64c7e36896a816bde4c5dc98f32b1be6a
Theshakyhorse <amaximus.sierra@gmail.com>
2026-02-07 18:22:13 -0800

    Second

--- main.cpp
+++ main.cpp
@@ -2,7 +2,26 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+struct Resturant {
+    string name;
+    int maxCapacity;
+    string typeOfCuisine;
+    string location;
+    bool full;
+};
 
+Resturant temp() {
+    Resturant one;
+    string n;
+    int m;
+    string t;
+    string l;
+    bool f;
+    cin >> n;
+
+}
+
+int main() {
+    Resturant first = temp();
     return 0;
 }
